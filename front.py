aaa0b342823ae96958350ef0927db9e34d5aaf48
WoosungMichael <woosung0420k@naver.com>
2021-06-07 01:33:14 +0900

    main

--- front.py
+++ front.py
@@ -1,7 +1,8 @@
 import search
 
+
 def start():
-        
+
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     print("■ ■                                                           ■ ■")
@@ -17,27 +18,31 @@ def start():
     print("■ ■                                                           ■ ■")
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
-    
-    print ("\n■ ■ ■ ■ ■ ■ ■ ◈  두루뭉술 논문 찾기  ◈  ■ ■ ■ ■ ■ ■ ■\n")
-#'\033[93m \033[100m'
+
+    print("\n■ ■ ■ ■ ■ ■ ■ ◈  두루뭉술 논문 찾기  ◈  ■ ■ ■ ■ ■ ■ ■\n")
+    # print('\033[93m \033[100m' + "\n■ ■ ■ ■ ■ ■ ■ ◈  두루뭉술 논문 찾기  ◈  ■ ■ ■ ■ ■ ■ ■\n")
+
 
 def searchedTitle(titles):
     for i in range(len(titles)):
-        print(i + 1, end = ". ")
+        print(i + 1, end=". ")
         print(titles[i]["title"])
 
-def searchedPaper(titles, title_num):
-    print("\n\n◈   Title   : ", end = "")
-    print(titles[int(title_num)-1]["title"])
-    print("\n◈  Abstract : ", end = "")
-    print(titles[int(title_num)-1]["content"], end = "\n\n\n\n")
 
-def paperRelation(titles, title_num):
+def searchedPaper(titles, title_num):
+    print("\n\n--------------------------")
+    print("◈  제목  : ", end="")
+    print(titles[int(title_num)-1]["title"])
+    print("\n◈  초록  ")
+    print(titles[int(title_num)-1]["content"], end="\n\n")
+
+
+def paperRelation(titles, title_index):
     print("\n----- 연관 논문 리스트 -----")
     index = 1
     for i in range(len(titles)):
-        if(int(title_num) - 1 != i):
-            print(index, end = ". (유사도 : ")
-            print("%0.2f" %(titles[i]["similarity"] * 100), end = "%) ")
+        if(int(title_index) != i):
+            print(index, end=". (유사도 : ")
+            print("%0.2f" % (titles[i]["similarity"] * 100), end="%) ")
             print(titles[i]["title"])
             index += 1
--- main.py
+++ main.py
@@ -1,60 +1,75 @@
 import front
 import search
+from operator import itemgetter
+import copy
 import levenshtein
 import nGram
-import Euclidean
+#import Euclidean
+
 
 def preprocess(text):
     return ''.join(filter(str.isalnum, text))
 
-title_list=[]
+
+title_list = []
 
 f = open("data.txt", 'rt', encoding='UTF8')
 
 while True:
     title = f.readline().rstrip()
-    #content = f.readline().rstrip()
-    content = "논문내용"
+    content = f.readline().rstrip()
+    #content = "논문내용"
     processed = preprocess(title)
-    
+
     if not title:
         break
-    title_list.append({'title' : title, 'content' : content, 'processed' : processed, 'similarity' : 0})
+    title_list.append({'title': title, 'content': content,
+                      'processed': processed, 'similarity': 0})
 
-f.close()    
+f.close()
 
 front.start()
 
 while True:
-    print("■ 검색할 키워드를 입력하세요 : ", end = "")
+    print("■ 검색할 키워드를 입력하세요 : ", end="")
     keyword = input()
     titles = search.search(title_list, keyword)
 
     print("\n----- 키워드가 포함된 논문 목록 -----")
     front.searchedTitle(titles)
 
-    
-    print("\n■ 읽을 논문의 번호를 입력하세요 : ", end = "")
+    print("\n■ 읽을 논문의 번호를 입력하세요 : ", end="")
     title_num = int(input())
     while(True):
         front.searchedPaper(titles, title_num)
 
-        #titles = levenshtein.levenshteion(titles, title_num)
-        #titles = nGram.nGram(titles, title_num)
+        #titles = levenshtein.levenshtein(titles, title_num)
+        titles = nGram.nGram(titles, title_num)
         #titles = Euclidean.euclidean(titles, title_num)
-        
-        front.paperRelation(titles, title_num)
 
-        print("\n■ 다음에 읽을 논문의 번호를 입력하세요(키워드 검색으로 돌아가기는 0, 종료는 q를 입력하세요) : ", end = "")
+        c_titles = copy.deepcopy(titles)
+
+        titles.sort(key=lambda x: x["similarity"], reverse=True)
+        for i in range(len(titles)):
+            if c_titles[title_num-1]["title"] == titles[i]["title"]:
+                title_index = i
+                break
+
+        front.paperRelation(titles, title_index)
+
+        print("\n■ 다음에 읽을 논문의 번호를 입력하세요(키워드 검색으로 돌아가기는 0, 종료는 q를 입력하세요) : ", end="")
         select = input()
         if select == 'q' or select == "Q":
             exit()
         elif select == '0':
+            print("\n")
             break
-        else:
+        elif 0 < int(select) < len(titles):
             select = int(select)
-            if(select < title_num):
+            if(select < title_index):
                 title_num = select
             else:
                 title_num = select + 1
-
+        else:
+            print("잘못된 input입니다.")
+            exit()
