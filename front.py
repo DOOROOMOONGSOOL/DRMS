c1e91f7cc29f1af5573cfcd9847bf3999151d9ae
WoosungMichael <woosung0420k@naver.com>
2021-06-06 21:51:51 +0900

    main

--- front.py
+++ front.py
@@ -1,52 +1,43 @@
 import search
 
 def start():
-    '''
+        
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     print("■ ■                                                           ■ ■")
-    print("■ ■     ■■■■■■                                                ■ ■")
-    print("■ ■     ■                                                     ■ ■")
-    print("■ ■     ■                                                     ■ ■")
-    print("■ ■     ■■■■■■                                                ■ ■")
-    print("■ ■                                                           ■ ■")
-    print("■ ■    ■■■■■■■■■                                              ■ ■")
+    print("■ ■     ■■■■■■      ■ ■ ■                                     ■ ■")
+    print("■ ■     ■           ■    ■                                    ■ ■")
+    print("■ ■     ■           ■    ■         ■      ■     ■ ■ ■ ■ ■     ■ ■")
+    print("■ ■     ■■■■■■      ■ ■ ■         ■ ■    ■ ■    ■             ■ ■")
+    print("■ ■                 ■   ■        ■   ■  ■   ■   ■ ■ ■ ■ ■     ■ ■")
+    print("■ ■    ■■■■■■■■■    ■    ■     ■      ■      ■          ■     ■ ■")
+    print("■ ■        ■        ■     ■    ■      ■      ■  ■ ■ ■ ■ ■     ■ ■")
     print("■ ■        ■                                                  ■ ■")
     print("■ ■        ■                                                  ■ ■")
-    print("■ ■        ■                                                  ■ ■")
-    print("■ ■                                                           ■ ■")
-    print("■ ■                                                           ■ ■")
     print("■ ■                                                           ■ ■")
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
     print("■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■")
-    '''
+    
     print ("\n■ ■ ■ ■ ■ ■ ■ ◈  두루뭉술 논문 찾기  ◈  ■ ■ ■ ■ ■ ■ ■\n")
+#'\033[93m \033[100m'
 
-
-def search1(titles):
+def searchedTitle(titles):
     for i in range(len(titles)):
-        if(titles[i][0] == titles[i][1]):
-            print(i + 1, end = ". ")
-            print(titles[i][0])
+        print(i + 1, end = ". ")
+        print(titles[i]["title"])
 
-        else:
-            print(i + 1, end = ". ")
-            print(titles[i][0], end = "  ( 번역 : ")
-            print(titles[i][1], end = " )")
-
-def search2(titles, title_num):
-    print("")
-    print("\n◈   Title   : ", end = "")
-    print(titles[int(title_num)-1][0])
+def searchedPaper(titles, title_num):
+    print("\n\n◈   Title   : ", end = "")
+    print(titles[int(title_num)-1]["title"])
     print("\n◈  Abstract : ", end = "")
-    print(titles[int(title_num)-1][2], end = "\n\n\n\n")
+    print(titles[int(title_num)-1]["content"], end = "\n\n\n\n")
 
+def paperRelation(titles, title_num):
     print("\n----- 연관 논문 리스트 -----")
     index = 1
     for i in range(len(titles)):
         if(int(title_num) - 1 != i):
             print(index, end = ". (유사도 : ")
-            #print(search.nGram(titles[int(title_num)-1][0], titles[i][0], 2), end = "%) ")
-            print(titles[i][0])
+            print("%0.2f" %titles[i]["similarity"] * 100, end = "%) ")
+            print(titles[i]["title"])
             index += 1
-            
--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 import front
 import search
+import levenshtein
+
+def preprocess(text):
+    return ''.join(filter(str.isalnum, text))
 
 title_list=[]
 
 f = open("data.txt", 'rt', encoding='UTF8')
 
 while True:
-    line1 = f.readline().rstrip()
-    #line2 = f.readline().rstrip()
-    line2 = "논문내용"
-    if not line1:
+    title = f.readline().rstrip()
+    #content = f.readline().rstrip()
+    content = "논문내용"
+    processed = preprocess(title)
+    
+    if not title:
         break
-    title_list.append([line1, line2])
+    title_list.append({'title' : title, 'content' : content, 'processed' : processed, 'similarity' : 0})
+
+f.close()    
 
 front.start()
 
-print("■ 검색할 키워드를 입력하세요 : ", end = "")
-keyword = input()
-titles = search.search(title_list, keyword)
+while True:
+    print("■ 검색할 키워드를 입력하세요 : ", end = "")
+    keyword = input()
+    titles = search.search(title_list, keyword)
 
-print("\n----- 키워드가 포함된 논문 목록 -----")
-front.search1(titles)
+    print("\n----- 키워드가 포함된 논문 목록 -----")
+    front.searchedTitle(titles)
 
-print("\n■ 읽을 논문의 번호를 입력하세요 : ", end = "")
-title_num = input()
-front.search2(titles, title_num)
+    
+    print("\n■ 읽을 논문의 번호를 입력하세요 : ", end = "")
+    title_num = int(input())
+    while(True):
+        front.searchedPaper(titles, title_num)
 
+        titles = levenshtein.levenshteion(titles, title_num)
+        front.paperRelation(titles, title_num)
 
-f.close()
+        print("\n■ 다음에 읽을 논문의 번호를 입력하세요(키워드 검색으로 돌아가기는 0, 종료는 q를 입력하세요) : ", end = "")
+        select = input()
+        if select == 'q' or select == "Q":
+            exit()
+        elif select == '0':
+            break
+        else:
+            if(int(select) < title_num):
+                title_num = select
+            else:
+                title_num = select + 1
+
